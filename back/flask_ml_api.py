1e806697a5a5c732c3827c1d919b15799ea84575
yurri775 <129472198+yurri775@users.noreply.github.com>
2024-04-02 01:25:43 +0100

    Update flask_ml_api.py
    J'ai une erreur dans le code il s'agit de ip adress

--- back/flask_ml_api.py
+++ back/flask_ml_api.py
@@ -10,7 +10,7 @@ cwd = CassandraReadWriteDb(ip_addrs=['172.25.198.7'], keyspace="emp")
 cwd.sync_class_table(PredictTxInfoModel)
 
 @app.route('/predict/tx', methods=['POST'])
-/*def create_task():
+def create_task():
     tx_data = request.json
     df = pd.DataFrame.from_records([tx_data])
     df = df.drop(['Time'],axis=1)
@@ -19,7 +19,7 @@ cwd.sync_class_table(PredictTxInfoModel)
     cwd.write_json_table(tx_data)
     tx_data['P'] = str(tx_data['P'])
     return jsonify(tx_data), 201
-/*
+
 
 if __name__ == '__main__':
     app.run(debug=True)
